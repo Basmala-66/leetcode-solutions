b55616817bd24595704365fdf7fb107644b0eb89
Basmala Mosaad <basmalamd55@gmail.com>
2026-02-18 14:52:39 +0200

    Time: 0 ms (100.00%) | Memory: 9 MB (8.43%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n ==0)
-         return 1;
-
-        else if( n == 1)
-        return x;
-
-        else
+       
         return pow(x,n);
 
     }
